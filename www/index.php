7908f8c2a510fc3bf1b60a3a8324854b64f78c38
mirodiro <109161979+mirodiro@users.noreply.github.com>
2022-07-21 13:44:50 -0400

    modif

--- www/index.php
+++ www/index.php
@@ -2,19 +2,39 @@
   
     define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true :false);
     $firstname =$lastname = $subject = $email= $message ="";
+    $firstnameError = $lastnameError =$subjectErrot = $emailError = "";
 
 //   var_dump($SERVER)
   if($_SERVER["REQUEST_METHOD"]  == "POST"){
-    $firstname = $POST["firstname"];
-    $lastname = $POST["lastname"];
-    $subject = $POST["subjec"];
-    $email = $POST["email"];
-    $message = $POST["masssage"];
+    $firstname = isset($POST["firstname"]) ?checkInput($_POST("firstname")) : "";
+    $lastname = isset($POST["lastname"])?checkInput($_POST(")lastname")) : "";
+    $subject = isset($POST["subjec"])?checkInput($_POST("subject")) : "";
+    $email = isset($POST["email"])?checkInput($_POST("email")) : "";
+    $message =isset( $POST["masssage"])?checkInput($_POST("massage")) : "";
+    if(!isEmail($email)){
+        $emailError = "Veillez verifiez votre email.";
+        echo  '<p class = "error">' .$emailError .'</p>';
+    }
 
   }else{
     if(IS_DEBUG){
     echo"pas post";
-  }
+    }
+}
+    function checkInput($input){
+    $input = trim($input);
+    $input=  stripcslashes($input);
+    $input= htmlspecialchars($input);
+    if(IS_DEBUG){
+        echo $input;
+        echo "br";
+    }
+
+    return $input;
+}
+
+function isEmail($email){
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 ?>
 <!doctype html>
@@ -29,12 +49,16 @@
 
 <body>
     <div id="formulaire">
+<!-- <form method="post" action="/tp_formulraire-de-contact/www/index.php/"><script>alert('bonjour')</script> -->
+<form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
         <form method="post" action="<?php $_SERVER["PHP_SELF"]      ?>">
-            <input type="email" placeholder="Prenom"name="firstname" required>
-            <input type="text" placeholder="nom"name="lastname" required>
-            <input type="text" placeholder="Sujet"name="subject" required>
-            <input type="text" placeholder="exmemple@email.com" name="email"required>
-            <textarea cols="30" rows="10" name="message" required></textarea>
+            <input type="text" placeholder="Prenom"name="firstname" value="<?php echo $firstname ?>"required>
+            <input type="text" placeholder="nom"name="lastname" value="<?php echo $lastname ?>"required>
+            <input type="text" placeholder="Sujet"name="subject"value="<?php echo $subject ?>" required>
+            <input type="email" placeholder="exmemple@email.com" name="email" value="<?php echo $email ?>" required>
+              
+             <!-- <p class=error>verifier votre message</p> -->
+            <textarea cols="30" rows="10" name="message"  value="<?php echo $message ?>"required></textarea>
             <input type="password" placeholder="mot de passe" required>
             <div id="select"> 
             <select name="date">

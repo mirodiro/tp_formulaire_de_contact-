a608516d9b85cb75d699a8e7010cd36c6d55c856
mirodiro <109161979+mirodiro@users.noreply.github.com>
2022-07-21 15:17:53 -0400

    modif

--- www/index.php
+++ www/index.php
@@ -7,10 +7,26 @@
 //   var_dump($SERVER)
   if($_SERVER["REQUEST_METHOD"]  == "POST"){
     $firstname = isset($POST["firstname"]) ?checkInput($_POST("firstname")) : "";
-    $lastname = isset($POST["lastname"])?checkInput($_POST(")lastname")) : "";
-    $subject = isset($POST["subjec"])?checkInput($_POST("subject")) : "";
+    if(empty($firstname)){
+        $firstnameError="pas post.";
+        }
+
+    $lastname = isset($POST["lastname"])?checkInput($_POST("lastname")) : "";
+    if(empty($lastname)){
+        $lastnameError="pas post.";
+        }
+    $subject = isset($POST["subject"])?checkInput($_POST("subject")) : "";
+    if(empty($subject)){
+        $subjectError="pas post.";
+        }
     $email = isset($POST["email"])?checkInput($_POST("email")) : "";
-    $message =isset( $POST["masssage"])?checkInput($_POST("massage")) : "";
+    if(empty($email)){
+        $emailError="pas post.";
+        }
+    $message =isset( $POST["messsage"])?checkInput($_POST("message")) : "";
+    if(empty($message)){
+        $messageError="pas post.";
+        }
     if(!isEmail($email)){
         $emailError = "Veillez verifiez votre email.";
         echo  '<p class = "error">' .$emailError .'</p>';
@@ -36,6 +52,11 @@
 function isEmail($email){
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
+function getError($error){
+    $html='<h2 class="error">' .$error.'</h2>';
+    return $html;
+
+}    
 ?>
 <!doctype html>
 <html lang="fr">
@@ -53,13 +74,41 @@ function isEmail($email){
 <form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
         <form method="post" action="<?php $_SERVER["PHP_SELF"]      ?>">
             <input type="text" placeholder="Prenom"name="firstname" value="<?php echo $firstname ?>"required>
-            <input type="text" placeholder="nom"name="lastname" value="<?php echo $lastname ?>"required>
-            <input type="text" placeholder="Sujet"name="subject"value="<?php echo $subject ?>" required>
-            <input type="email" placeholder="exmemple@email.com" name="email" value="<?php echo $email ?>" required>
-              
-             <!-- <p class=error>verifier votre message</p> -->
-            <textarea cols="30" rows="10" name="message"  value="<?php echo $message ?>"required></textarea>
-            <input type="password" placeholder="mot de passe" required>
+           
+            <?php                 
+              echo !IS_DEBUG ? "required":""?>>
+           <?php                 
+               if($firstnameError !=""){ 
+               echo getError($firstnameError); }
+             ?>
+            <input type="text" placeholder="nom"name="lastname" value="<?php echo $lastname ?>"<?php                 
+              echo !IS_DEBUG ? "required":""?>
+             ?>
+            <input type="text" placeholder="Sujet"name="subject"value="<?php echo $subject ?>" <?php                 
+              echo !IS_DEBUG ? "required":""?>
+             ?>
+            <input type="email" placeholder="exmemple@email.com" name="email" value="<?php echo $email ?>" <?php                 
+              echo !IS_DEBUG ? "required":""?>
+             ?>
+            <?php 
+           if($emailError!=""){
+                   echo getError($emailError);
+           }
+           
+            ?>
+              <!-- // <p class="error">verifier votre message</p> -->
+            <textarea cols="30"placeholerd="Tapez votre message." rows="10" name="message">
+            
+            <?php echo!IS_DEBUG "required":""?>
+              <?php echo $message </textarea>?>
+
+              <?php
+           if($emailError!=""){
+                   echo getError($emailError);
+           }
+           
+            ?>
+            <!-- <input type="password" placeholder="mot de passe" required>
             <div id="select"> 
             <select name="date">
                 <option>1</option>
@@ -84,6 +133,6 @@ function isEmail($email){
             </select>
             </div>
             <input type="submit" value="ENVOYER">
-        </form>
+        </form> -->
     </div>
 </body></html>
